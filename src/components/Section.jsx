820bac8d5e60caeb85170aaabc0250b50b272d83
ctrl1758 <franco.more1758@gmail.com>
2024-07-21 23:13:59 -0300

    nosotros 90% completo, falta poner la foto de gabi.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,7 +11,10 @@ import {
 } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import PreviewIcon from "@mui/icons-material/Preview";
+import { CheckIcon } from "@heroicons/react/outline";
+import { FaGithub, FaEye } from "react-icons/fa";
 import img from "../assets/imgApp.webp";
+import franco from "../assets/franco.jpg";
 import svg1 from "../assets/imgsection2.svg";
 
 function Section({ title, id, children, gradientClass }) {
@@ -74,40 +77,146 @@ function Section({ title, id, children, gradientClass }) {
             </div>
           </>
         ) : id === "nosotros" ? (
-          <>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div className="col-span-1">
-                <Typography
-                  variant="h1"
-                  sx={{ fontSize: "4rem", textTransform: "capitalize" }}
-                  className="text-gradient"
-                >
-                  {title}
-                </Typography>
-                <Typography variant="body1">
-                  Este es nuestro equipo de trabajo!
-                </Typography>
-                <div className="p-4 flex-1">
-                  <img
-                    src={svg1}
-                    alt="Imagen"
-                    className="w-full h-full object-cover"
-                  />
+          <div className="min-h-screen flex items-center justify-center">
+            <div
+              className="flex flex-wrap w-full max-w-screen-lg"
+              style={{ height: "80vh" }}
+            >
+              <div className="flex-1 min-w-[300px] p-4 flex flex-col justify-around">
+                <div>
+                  <Typography
+                    variant="h1"
+                    sx={{ fontSize: "4rem", textTransform: "capitalize" }}
+                    className="text-gradient"
+                  >
+                    {title}
+                  </Typography>
+                  <Typography variant="body1">
+                    Este es nuestro equipo de trabajo!
+                  </Typography>
+                </div>
+                <div className="p-4 flex-col flex justify-end">
+                  <img src={svg1} alt="Imagen" className="" />
                 </div>
               </div>
-              <div className="col-span-1 flex flex-col gap-4">
+              <div className="flex-1 min-w-[300px] p-4 flex flex-col gap-4 justify-center">
                 <div className="bg-gray-700 p-4 flex-1">
-                  <Typography variant="h6">Div de Gabi</Typography>
-                  <Typography variant="body1">Contenido del Gabi.</Typography>
+                  <div className="flex justify-between items-center">
+                    <Typography variant="h6">Gabriel Correa</Typography>
+                    <img
+                      alt=""
+                      src=""
+                      className="inline-block h-20 w-20 rounded-full ring-2 ring-white"
+                    />
+                    <Typography variant="h6">Full Stack WEB</Typography>
+                  </div>
+                  <div className="flex justify-between mt-4 items-end">
+                    <ul className="list-none ml-4 mt-2">
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        HTML-CSS-JavaScript
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        React
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        Tailwind, Material UI
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        NodeJS
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        Google Cloud Services
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        OPEN AI
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        MP, Stripe, Paypal
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />4
+                        Años de experiencia freelance
+                      </li>
+                    </ul>
+                    <div className="ml-4 mt-2 flex flex-col gap-2 h-full">
+                      <button className="flex items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                        <FaGithub className="mr-2" />
+                        GitHub
+                      </button>
+                      <button className="flex items-center bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+                        <FaEye className="mr-2" />
+                        Preview
+                      </button>
+                    </div>
+                  </div>
                 </div>
-
                 <div className="bg-gray-700 p-4 flex-1">
-                  <Typography variant="h6">Div de Franco</Typography>
-                  <Typography variant="body1">Contenido del Franco.</Typography>
+                  <div className="flex justify-between items-center">
+                    <Typography variant="h6">Franco More</Typography>
+                    <img
+                      alt=""
+                      src={franco}
+                      className="inline-block h-20 w-20 rounded-full ring-2 ring-white"
+                    />
+                    <Typography variant="h6">Full Stack WEB</Typography>
+                  </div>
+                  <div className="flex justify-between mt-4 items-end">
+                    <ul className="list-none ml-4 mt-2">
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        HTML-CSS-JavaScript
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        React
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        Tailwind, Material UI
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        NodeJS
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        Google Cloud Services
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        OPEN AI
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />
+                        MP, Stripe, Paypal
+                      </li>
+                      <li className="flex items-center">
+                        <CheckIcon className="h-5 w-5 text-green-500 mr-2" />4
+                        Años de experiencia freelance
+                      </li>
+                    </ul>
+                    <div className="ml-4 mt-2 flex flex-col gap-2 h-full">
+                      <button className="flex items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                        <FaGithub className="mr-2" />
+                        GitHub
+                      </button>
+                      <button className="flex items-center bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+                        <FaEye className="mr-2" />
+                        Preview
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </>
+          </div>
         ) : (
           children || (
             <Typography variant="h4" className="text-2xl">
